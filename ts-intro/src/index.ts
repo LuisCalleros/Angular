ff3a4539a62da938b26657ac486ca1a586811616
luis Alberto Calleros Carrillo <calleros_jr@hotmail.com>
2022-05-13 18:27:36 -0500

    ash

---
+++ ts-intro/src/index.ts
@@ -0,0 +1,22 @@
+ interface Producto{
+     desc:string, 
+     precio: number,
+ }
+
+ const telefono: Producto = {
+     desc:'Nokia l',
+     precio: 2000
+ }
+ const tableta: Producto ={
+     desc: "Ipad",
+     precio:5000
+ }
+
+ function calculaISV( productos: Producto[] ){
+    let total =0;
+
+    productos.forEach( (producto: Producto ) =>{
+
+    } )
+
+ }
