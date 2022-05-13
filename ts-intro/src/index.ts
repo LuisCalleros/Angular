f251ce98e442e3ce110127b934958ed864aeb291
luis Alberto Calleros Carrillo <calleros_jr@hotmail.com>
2022-05-13 14:38:16 -0500

    clase 3

--- ts-intro/src/index.ts
+++ ts-intro/src/index.ts
@@ -7,3 +7,17 @@
 // let habilidades :string[] = ['bash', ' Conuter', 'Healing'];
 
 
+function sumar(a: number, b: number): number {
+    return a + b;
+}
+
+const sumarFlecha = (a: number, b:number):number =>{
+    return a + b;
+}
+
+
+
+const resul = sumarFlecha(5, 4);
+console.log(resul);
+
+
