f87264d65b98573fe6e94d98c9f0adeb35c73525
luis Alberto Calleros Carrillo <calleros_jr@hotmail.com>
2022-05-16 09:39:36 -0500

    corre

--- ts-intro/src/ejercicios/06-desestructar-fution.ts
+++ ts-intro/src/ejercicios/06-desestructar-fution.ts
@@ -1,31 +1,30 @@
- interface Producto{
-     desc:string, 
-     precio: number,
- }
+interface Producto{
+    desc:string, 
+    precio: number,
+}
 
- const telefono: Producto = {
-     desc:'Nokia l',
-     precio: 150
- }
- const tableta: Producto ={
-     desc: "Ipad",
-     precio:350
- }
+const telefono: Producto = {
+    desc:'Nokia l',
+    precio: 150
+}
+const tableta: Producto ={
+    desc: "Ipad",
+    precio:350
+}
 
- function calculaISV( productos: Producto[] ): [ number, number ]{
-    let total =0;
+function calculaISV( productos: Producto[] ): [ number, number ]{
+   let total =0;
 
-    productos.forEach( ( { precio } ) =>{
-        total += precio;
-    } )
+   productos.forEach( ( { precio } ) =>{
+       total += precio;
+   } )
 
-    return [total, total*0.15];
- }
+   return [total, total*0.15];
+}
 
- const articulos = [ telefono, tableta ];
+const articulos = [ telefono, tableta ];
 
- const [total, isv] = calculaISV(articulos);
+const [total, isv] = calculaISV(articulos);
 
- console.log('Total: ', total);
- console.log('IVS: ', isv);
- 
+console.log('Total: ', total);
+console.log('IVS: ', isv);
